c7a479a310ea4facb547396a8a63e7e6588b384f
eomgerm <fishbread00@gmail.com>
2024-03-13 10:41:03 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/goormthonuniv/ownearth/OwnEarthApplication.java
@@ -0,0 +1,13 @@
+package com.goormthonuniv.ownearth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnEarthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnEarthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goormthonuniv/ownearth/OwnEarthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthonuniv.ownearth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnEarthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
