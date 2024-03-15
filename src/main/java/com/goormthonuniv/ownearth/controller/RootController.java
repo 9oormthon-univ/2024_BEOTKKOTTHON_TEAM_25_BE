11b4ccdc76fb5402e73c2209142de7768ada8b7e
eomgerm <fishbread00@gmail.com>
2024-03-15 10:33:13 +0900

    :recycle: [Refactor]: 코드 컨벤션 적용

--- src/main/java/com/goormthonuniv/ownearth/controller/RootController.java
+++ src/main/java/com/goormthonuniv/ownearth/controller/RootController.java
@@ -1,10 +1,11 @@
 package com.goormthonuniv.ownearth.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/")
