e215247c34b928fbcba324463d6a916b313a9d28
eomgerm <fishbread00@gmail.com>
2024-03-15 15:37:50 +0900

    :sparkles: [Feat]: 비밀번호 wrapper 클래스 구현

---
+++ src/main/java/com/goormthonuniv/ownearth/domain/member/Password.java
@@ -0,0 +1,34 @@
+package com.goormthonuniv.ownearth.domain.member;
+
+import java.util.regex.Pattern;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.goormthonuniv.ownearth.exception.GlobalErrorCode;
+import com.goormthonuniv.ownearth.exception.MemberException;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class Password {
+  private static final String PASSWORD_REGEX =
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#%^&*()_+-=\\[\\]{}|;':\",./<>?~`\\\\])[A-Za-z\\d!@#%^&*()_+\\-=\\[\\]{}|;':\",./<>?~`\\\\]{9,16}";
+
+  private final String encryptedPassword;
+
+  public static Password encrypt(String plainPassword, BCryptPasswordEncoder encoder) {
+    if (!isPasswordValid(plainPassword)) {
+      throw new MemberException(GlobalErrorCode.NOT_VALID_PASSWORD);
+    }
+    return new Password(encoder.encode(plainPassword));
+  }
+
+  public static Boolean isPasswordValid(String plainPassword) {
+    return Pattern.matches(PASSWORD_REGEX, plainPassword);
+  }
+
+  public Boolean isSamePassword(String plainPassword, BCryptPasswordEncoder passwordEncoder) {
+    return passwordEncoder.matches(plainPassword, encryptedPassword);
+  }
+}
