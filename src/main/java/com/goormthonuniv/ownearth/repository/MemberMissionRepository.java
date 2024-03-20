ff49418814cbfd58420b3a05e4a021307d972e18
SIWON990327 <pos06058@naver.com>
2024-03-20 14:08:27 +0900

    :recycle:[Refactor]: 충돌해결

--- src/main/java/com/goormthonuniv/ownearth/repository/MemberMissionRepository.java
+++ src/main/java/com/goormthonuniv/ownearth/repository/MemberMissionRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.goormthonuniv.ownearth.domain.enums.MissionCategory;
 import com.goormthonuniv.ownearth.domain.mapping.MemberMission;
@@ -20,16 +19,9 @@ public interface MemberMissionRepository extends JpaRepository<MemberMission, Lo
 
   Integer countByMemberAndCompletedAtBetween(Member member, LocalDateTime start, LocalDateTime end);
 
-<<<<<<< HEAD
   List<MemberMission> findAllByMemberAndCompletedAtBetween(
       Member member, LocalDateTime start, LocalDateTime end);
 
   List<MemberMission> findAllByMemberAndMission_MissionCategoryAndIsCompletedTrue(
       Member member, MissionCategory missionCategory);
-=======
-  @Query(
-      "SELECT mm FROM MemberMission mm WHERE mm.member.id = :memberId AND mm.createdAt >= :start AND mm.createdAt <= :end")
-  Optional<MemberMission> findMemberMissionByMemberIdAndDate(
-      Long memberId, LocalDateTime start, LocalDateTime end);
->>>>>>> 0475893bfbf301a9f0d6cb8b543fe7ffb7203324
 }
