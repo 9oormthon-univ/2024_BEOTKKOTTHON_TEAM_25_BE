b52135f5976012abd8dfb289cc3f5460f9db3e46
eomgerm <fishbread00@gmail.com>
2024-03-20 12:24:45 +0900

    :recycle: [Refactor]: 기본값 설정

--- src/main/java/com/goormthonuniv/ownearth/domain/mapping/Friend.java
+++ src/main/java/com/goormthonuniv/ownearth/domain/mapping/Friend.java
@@ -1,11 +1,21 @@
 package com.goormthonuniv.ownearth.domain.mapping;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 import com.goormthonuniv.ownearth.domain.common.BaseEntity;
 import com.goormthonuniv.ownearth.domain.member.Member;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -18,7 +28,7 @@ public class Friend extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private Boolean isFriend;
+  @Builder.Default private Boolean isFriend = false;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "from_member_id")
