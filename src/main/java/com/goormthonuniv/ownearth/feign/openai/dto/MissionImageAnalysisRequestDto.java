efdce87255f8b4cdf3ca8f7129b777978d818c4a
eomgerm <fishbread00@gmail.com>
2024-03-23 19:13:44 +0900

    :recycle: [Refactor]: AI 프롬프트 리팩토링

--- src/main/java/com/goormthonuniv/ownearth/feign/openai/dto/MissionImageAnalysisRequestDto.java
+++ src/main/java/com/goormthonuniv/ownearth/feign/openai/dto/MissionImageAnalysisRequestDto.java
@@ -9,13 +9,17 @@ import org.springframework.web.multipart.MultipartFile;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.goormthonuniv.ownearth.feign.openai.Prompt;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 public class MissionImageAnalysisRequestDto {
+
   private String model = "gpt-4-vision-preview";
   private List<Message> messages;
 
@@ -24,29 +28,55 @@ public class MissionImageAnalysisRequestDto {
   }
 
   @Getter
+  @AllArgsConstructor(access = AccessLevel.PROTECTED)
   public static class Message {
-    private String role = "user";
-    private List<Content> content;
 
-    public Message(List<Content> contentList) {
-      this.content = contentList;
-    }
+    private Role role;
+    private List<Content> content;
+  }
+
+  @RequiredArgsConstructor
+  @Getter
+  public enum Role {
+    USER("user"),
+    SYSTEM("system");
+
+    @JsonValue private final String value;
   }
 
   @AllArgsConstructor(access = AccessLevel.PROTECTED)
   @Getter
   @JsonInclude(Include.NON_NULL)
   public static class Content {
-    private String type;
+
+    private Type type;
     private String text;
 
     @JsonProperty("image_url")
     private ImageUrl imageUrl;
+
+    public static Content textContent(String text) {
+      return new Content(Type.TEXT, text, null);
+    }
+
+    public static Content imageContent(String imageUrl) {
+      return new Content(Type.IMAGE_URL, null, new ImageUrl(imageUrl));
+    }
+  }
+
+  @RequiredArgsConstructor
+  @Getter
+  public enum Type {
+    TEXT("text"),
+    IMAGE_URL("image_url");
+
+    @JsonValue private final String value;
   }
 
   @AllArgsConstructor(access = AccessLevel.PROTECTED)
   @Getter
   public static class ImageUrl {
+
     private String url;
   }
 
@@ -54,14 +84,21 @@ public class MissionImageAnalysisRequestDto {
       throws IOException {
     String base64Image = Base64.getEncoder().encodeToString(image.getBytes());
     String imageUrl = String.format("data:%s;base64,%s", image.getContentType(), base64Image);
-    List<Content> contentList =
-        List.of(
-            new Content(
-                "text",
-                "이 이미지가 \"" + mission + "\"라는 미션을 수행하는 이미지입니까? 부가적인 설명 없이 오직 true 혹은 false로만 답하시오.",
-                null),
-            new Content("image_url", null, new ImageUrl(imageUrl)));
+    return new MissionImageAnalysisRequestDto(createMessages(mission, imageUrl));
+  }
 
-    return new MissionImageAnalysisRequestDto(List.of(new Message(contentList)));
+  private static List<Message> createMessages(String mission, String imageUrl) {
+    return List.of(
+        new Message(Role.SYSTEM, createSystemContents()),
+        new Message(Role.USER, createUserContents(mission, imageUrl)));
+  }
+
+  private static List<Content> createSystemContents() {
+    return List.of(Content.textContent(Prompt.getSystemPrompt()));
+  }
+
+  private static List<Content> createUserContents(String mission, String imageUrl) {
+    return List.of(
+        Content.textContent(Prompt.getUserPrompt(mission)), Content.imageContent(imageUrl));
   }
 }
